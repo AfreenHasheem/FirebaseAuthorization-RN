3d85ae7b206610df27f324bcff82086b0cb0729b
Siraj <asrf@C02SG255G8WN.local>
2019-03-06 14:43:16 +0000

    Showing spinner after checking switch statements

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Card, CardSection } from './components/common';
+import { Header, Button, Card, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { loggedIn: false };
-  componentWillMount() {
-  firebase.initializeApp({
-      apiKey: 'AIzaSyBZ6Jp9o7kpgrPFCYY89vjq_96ZqzWRJac',
-      authDomain: 'authentication-d3382.firebaseapp.com',
-      databaseURL: 'https://authentication-d3382.firebaseio.com',
-      projectId: 'authentication-d3382',
-      storageBucket: 'authentication-d3382.appspot.com',
-      messagingSenderId: '999130161220'
+  state = { loggedIn: null };//null cuz we dont know if we are signed in or not
+    componentWillMount() {
+    firebase.initializeApp({
+        apiKey: 'AIzaSyBZ6Jp9o7kpgrPFCYY89vjq_96ZqzWRJac',
+        authDomain: 'authentication-d3382.firebaseapp.com',
+        databaseURL: 'https://authentication-d3382.firebaseio.com',
+        projectId: 'authentication-d3382',
+        storageBucket: 'authentication-d3382.appspot.com',
+        messagingSenderId: '999130161220'
   });
  firebase.auth().onAuthStateChanged((user) => {
    if (user) {
@@ -25,24 +25,29 @@ class App extends Component {
   }
 
   renderContent() {
-    if (this.state.loggedIn) {
-      //if the user is logged in show Logout btn
-      return (
-        /*initially the button was spread across the page
-        so added it inside Card and CardSection to format the
-        size of the button*/
-        <Card>
-          <CardSection>
-            <Button>
-             Log Out
-            </Button>
-          </CardSection>
-        </Card>
-      );
+    switch (this.state.loggedIn) {
+      /*initially the button was spread across the page
+      so added it inside Card and CardSection to format the
+      size of the button*/
+      case true:
+      return (<Card>
+        <CardSection>
+          <Button>
+           Log Out
+          </Button>
+        </CardSection>
+      </Card>);
+
+      case false:
+        return <LoginForm />;
+
+      default:
+        return <Spinner size="large" />
+
     }
-    //if user is not loggedin show login form
-    return <LoginForm />;
   }
+    //if user is not loggedin show login form
+
   render() {
     return (
       <View style={{ backgroundColor: 'white', flex: 1 }}>
--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -23,4 +23,4 @@ const styles = {
 
 };
 
-export {CardSection};
+export { CardSection };
